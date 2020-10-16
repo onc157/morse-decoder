f1d0e8768609515beb271b8e83ac93df2a965cf7
onc157 <onc157@yandex.ru>
2020-10-16 23:49:00 +0300

    solved the morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let binaryWords = {};
+    let result = '';
+
+    for (let key in MORSE_TABLE) {  // Create hash table. Key - symbal; Value - morse code;
+      for (let char of key) {
+        if (binaryWords[MORSE_TABLE[key]] === undefined) {
+          binaryWords[MORSE_TABLE[key]] = ''
+        }
+        if (char === '.') {
+          binaryWords[MORSE_TABLE[key]] += '10';
+        } else if (char === '-') {
+          binaryWords[MORSE_TABLE[key]] += '11';
+        }
+      }
+
+      //Fill empty cells with zeros
+      binaryWords[MORSE_TABLE[key]] = binaryWords[MORSE_TABLE[key]].padStart(10, 0)
+
+    }
+
+    // add space code in 'binaryWords'
+    binaryWords[' '] = '**********'
+
+    let morseBinary = expr;
+    for (let i = 0; i <= expr.length / 10; i = i + 1) { // Decrypt morse binary code
+      let = current = morseBinary.slice(0, 10);
+      for (let key in binaryWords) {
+            if (current === binaryWords[key]) {
+              result += key;
+              morseBinary = morseBinary.slice(10);
+            }
+      }
+    }
+    return result;
 }
 
 module.exports = {
